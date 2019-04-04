5e5514a2dcb21c540a4051a36883eb816791c373
Deepak Kumar Mishra <DM00601366@techmahindra.com>
2019-04-04 14:01:23 +0530

    initial code commit

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    font-family: 'Open Sans', sans-serif;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 500px;
+    height: 40%;
+`;
+
+const Title = styled.h1`
+    color: black;
+    font-size: 2.5rem;
+    font-weight: 700;
+`;
+
+const Paragraph = styled.p`
+    color: black;
+    font-size: 1rem;
+`;
+
+const Link = styled.a`
+    color: #00a8e8;
+`;
+
+const App = () => (
+    <Container>
+        <Wrapper>
+            <Title>
+            PSTN Dahboard
+            </Title>
+            <Paragraph>This is new PSTN Dahboard boilerplate.</Paragraph>
+            <Paragraph>
+                Test Link{' '}
+                <Link href="soon" target="_blank">
+                    here.
+                </Link>
+            </Paragraph>
+        </Wrapper>
+    </Container>
+);
+
+export default App;
